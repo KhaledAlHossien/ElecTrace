293e04ef8dd6a60fe9a478ff31decf8da30f27d1
Homam Nasser <homam.naser.3@gmail.com>
2026-07-22 12:38:57 +0300

    Edit MeterReadings

--- Application Contract/DTOs/MeterReading/CreateMeterReadingRequestDto.cs
+++ Application Contract/DTOs/MeterReading/CreateMeterReadingRequestDto.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Application_Contract.DTOs.MeterReading
 {
     public record CreateMeterReadingRequestDto(
-        int DepartmentId,
+        string MeterCode,
         Months Month,
         decimal CurrentReading
     );
